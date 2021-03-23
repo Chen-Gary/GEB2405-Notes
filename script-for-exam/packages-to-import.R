e80fc6f0671486c7560ec795f827bb85e41892a3
Gary Chen <chenhhgary@gmail.com>
2021-03-23 17:57:42 +0800

    small change on packages-to-import.R

--- script-for-exam/packages-to-import.R
+++ script-for-exam/packages-to-import.R
@@ -4,5 +4,6 @@
 library(psych) # describe()
 library(dplyr) # na_if() and recode() - lec3
 
+library(gmodels) # for `CrossTable()` -- pie chart - lec7
 library(ggplot2)
 library(forcats) # fct_infreq(x)
